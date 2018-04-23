b36e98dc282507994cae6543b2700faf627e9d26
sereno1987 <33984299+sereno1987@users.noreply.github.com>
2018-04-23 15:58:11 +0430

    adding auto page detection

--- real-state-scrapping.py
+++ real-state-scrapping.py
@@ -1,56 +1,67 @@
-#
 import requests
 from bs4 import BeautifulSoup
 import pandas
 
-# load a webpage
-r = requests.get ( "https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+# load first page
+r = requests.get("https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS")
 c = r.content
-soup=BeautifulSoup(c , "html.parser")
-# print(soup.prettify())
+soup = BeautifulSoup(c, "html.parser")
+# page numbers
+page_num = soup.find_all("a", {"class": "Page"})[-1].text
+print(page_num)
+# define a list and base URL
+l = [ ]
+base_url = "https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/t=0&s="
+# loop all pages
+for page in range(0, int(page_num)*10, 10):
 
-# define list
-l=[]
-# for the first item in list use the find method or the [0] item
-all = soup.find_all("div",{"class":"propertyRow"})
-#print(all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
-for item in all:
-    d={}
-    d["Price"]=item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
-    d["Address"]=item.find_all("span",{"class":"propAddressCollapse"})[0].text
-    d["Locality" ]=item.find_all("span",{"class":"propAddressCollapse"})[1].text
+    # load a webpage
+    r = requests.get ( base_url + str ( page ) + ".html")
+    c = r.content
+    soup=BeautifulSoup(c , "html.parser")
+    # print(soup.prettify())
 
-# for infoBed if the answer is None so .text will lead to an error, so using try and except can help!
-    try:
-        d["Bed"] = item.find("span", {"class": "infoBed"}).find("b").text
-    except:
-        d["Bed"] = None  # pass or print(None)
-    try:
-        d["Area"] = item.find("span", {"class": "infSqFt"}).find("b").text
-    except:
-        d["Area"] = None
-    try:
-        d["Full Bath"] = item.find("span", {"class": "infoValueFullBath"}).find("b").text
-    except:
-        d["Full Bath"] = None
-    try:
-        d["Half Bed"] = item.find("span", {"class": "infoValueHalfBath"}).find("b").text
-    except:
-        d["Half Bed"] = None
-    try:
-        for columnGroup in item.find_all("div",{"class":"columnGroup"}):
-            #print(columnGroup)
-            for featureGroup, featureName in zip(columnGroup.find_all("span",{"class":"featureGroup"}),columnGroup.find_all("span",{"class":"featureName"})):
-                #if featureGroup.text == "Lot Size":
-                    if "Lot Size" in featureGroup.text:
-                        d["Lot Size"] = featureName.text
+    # for the first item in list use the find method or the [0] item
+    all = soup.find_all("div",{"class":"propertyRow"})
 
-    except:
-        d["Lot Size"] = None
+    #print(all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
+    for item in all:
+        d={}
+        d["Price"]=item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+        d["Address"]=item.find_all("span",{"class":"propAddressCollapse"})[0].text
+        d["Locality" ]=item.find_all("span",{"class":"propAddressCollapse"})[1].text
 
-    l.append(d)
+    # for infoBed if the answer is None so .text will lead to an error, so using try and except can help!
+        try:
+            d["Bed"] = item.find("span", {"class": "infoBed"}).find("b").text
+        except:
+            d["Bed"] = None  # pass or print(None)
+        try:
+            d["Area"] = item.find("span", {"class": "infSqFt"}).find("b").text
+        except:
+            d["Area"] = None
+        try:
+            d["Full Bath"] = item.find("span", {"class": "infoValueFullBath"}).find("b").text
+        except:
+            d["Full Bath"] = None
+        try:
+            d["Half Bed"] = item.find("span", {"class": "infoValueHalfBath"}).find("b").text
+        except:
+            d["Half Bed"] = None
+        try:
+            for columnGroup in item.find_all("div",{"class":"columnGroup"}):
+                #print(columnGroup)
+                for featureGroup, featureName in zip(columnGroup.find_all("span",{"class":"featureGroup"}),columnGroup.find_all("span",{"class":"featureName"})):
+                    #if featureGroup.text == "Lot Size":
+                        if "Lot Size" in featureGroup.text:
+                            d["Lot Size"] = featureName.text
 
-# define data frame
+        except:
+            d["Lot Size"] = None
+
+        l.append(d)
+
+    # define data frame
 df=pandas.DataFrame(l)
 df.to_csv("out.csv")
 
